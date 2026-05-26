22f51d51b2db5b108b385df74f0c5814182587a2
Akansha Roy <akansharoy2008@gmail.com>
2026-05-26 19:57:19 +0530

    feat(internal/token) : supporting tokenisation for mini proto compiler

--- internal/token/token.go
+++ internal/token/token.go
@@ -1,1 +1,53 @@
 package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL TokenType = "ILLEGAL"
+	EOF     TokenType = "EOF"
+
+	//identifiers and literals
+	IDENT  TokenType = "IDENT"
+	NUMBER TokenType = "NUMBER"
+	STRING TokenType = "STRING"
+
+	//operators
+	ASSIGN TokenType = "="
+
+	//delimiters
+	SEMICOLON TokenType = ";"
+	LPAREN    TokenType = "("
+	RPAREN    TokenType = ")"
+	LBRACE    TokenType = "{"
+	RBRACE    TokenType = "}"
+	DOT       TokenType = "."
+
+	//keywords
+	MESSAGE TokenType = "MESSAGE"
+	SERVICE TokenType = "SERVICE"
+	RPC     TokenType = "RPC"
+	RETURNS TokenType = "RETURNS"
+	SYNTAX  TokenType = "SYNTAX"
+	PACKAGE TokenType = "PACKAGE"
+)
+
+var keywords = map[string]TokenType{
+	"message": MESSAGE,
+	"service": SERVICE,
+	"rpc":     RPC,
+	"returns": RETURNS,
+	"syntax":  SYNTAX,
+	"package": PACKAGE,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
