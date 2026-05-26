9bc9572e29cc1f90b3a9710d154bb93da8e71757
Akansha Roy <akansharoy2008@gmail.com>
2026-05-26 20:00:42 +0530

    feat(internal/lexer) : implemented lexer with token validation

--- internal/lexer/lexer.go
+++ internal/lexer/lexer.go
@@ -1,1 +1,140 @@
 package lexer
+
+import (
+	"github.com/akansha204/mini-protoc/internal/token"
+)
+
+type Lexer struct {
+	input        string
+	position     int //points to curr
+	readPosition int //points to the next char
+	ch           byte
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{
+		input: input,
+	}
+	l.readChar()
+	return l
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition++
+}
+
+func (l *Lexer) peekChar() byte {
+	if l.readPosition >= len(l.input) {
+		return 0
+	}
+	return l.input[l.readPosition]
+}
+
+func (l *Lexer) skipComment() {
+	for l.ch != '\n' && l.ch != 0 {
+		l.readChar()
+	}
+}
+
+func NewToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isIdentifierChar(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+func isIdentifierChar(ch byte) bool {
+	return isLetter(ch) || isDigit(ch)
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+
+func (l *Lexer) readNumber() string {
+	position := l.position
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
+}
+
+func (l *Lexer) readString() string {
+	position := l.position + 1
+	for {
+		l.readChar()
+		if l.ch == '"' || l.ch == 0 {
+			break
+		}
+	}
+	return l.input[position:l.position]
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	l.skipWhitespace()
+
+	if l.ch == '/' && l.peekChar() == '/' {
+		l.skipComment()
+		return l.NextToken()
+	}
+
+	switch l.ch {
+	case '=':
+		tok = NewToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = NewToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = NewToken(token.LPAREN, l.ch)
+	case ')':
+		tok = NewToken(token.RPAREN, l.ch)
+	case '{':
+		tok = NewToken(token.LBRACE, l.ch)
+	case '}':
+		tok = NewToken(token.RBRACE, l.ch)
+	case '.':
+		tok = NewToken(token.DOT, l.ch)
+	case '"':
+		tok.Type = token.STRING
+		tok.Literal = l.readString()
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			return tok
+		} else if isDigit(l.ch) {
+			tok.Type = token.NUMBER
+			tok.Literal = l.readNumber()
+			return tok
+		} else {
+			tok = NewToken(token.ILLEGAL, l.ch)
+		}
+	}
+	l.readChar()
+	return tok
+}
