dab0e287deb5496b9dade858433fc2b65d5c2b85
Akansha Roy <akansharoy2008@gmail.com>
2026-05-31 16:22:50 +0530

    feat(internal/ast): ast design

--- internal/ast/ast.go
+++ internal/ast/ast.go
@@ -1,1 +1,31 @@
 package ast
+
+type ProtoFile struct {
+	Syntax  string
+	Package string
+
+	Messages []*Message
+	Services []*Service
+}
+
+type Message struct {
+	Name   string
+	Fields []*Field
+}
+
+type Field struct {
+	Type   string
+	Name   string
+	Number int
+}
+
+type Service struct {
+	Name string
+	RPC  []*RPC
+}
+
+type RPC struct {
+	Name         string
+	RequestType  string
+	ResponseType string
+}
