296b8506428d3fbd6ffb118aaf32c71596922bad
Akansha Roy <akansharoy2008@gmail.com>
2026-06-06 20:01:43 +0530

    test : manual protofile test

--- cmd/mini-protoc/main.go
+++ cmd/mini-protoc/main.go
@@ -1,1 +1,64 @@
 package main
+
+import (
+	"fmt"
+
+	"github.com/akansha204/mini-protoc/internal/ast"
+	"github.com/akansha204/mini-protoc/internal/generator"
+	"github.com/akansha204/mini-protoc/internal/validator"
+)
+
+func main() {
+	proto := &ast.ProtoFile{
+		Syntax:  "proto3",
+		Package: "user",
+
+		Messages: []*ast.Message{
+			{
+				Name: "User",
+				Fields: []*ast.Field{
+					{
+						Type:   "string",
+						Name:   "name",
+						Number: 1,
+					},
+					{
+						Type:   "int32",
+						Name:   "age",
+						Number: 2,
+					},
+				},
+			},
+			{
+				Name: "Person",
+				Fields: []*ast.Field{
+					{
+						Type:   "string",
+						Name:   "name",
+						Number: 1,
+					},
+					{
+						Type:   "int32",
+						Name:   "age",
+						Number: 2,
+					},
+				},
+			},
+		},
+	}
+
+	v := validator.New()
+	v.ValidateProtoFile(proto)
+
+	if len(v.Errors()) > 0 {
+		for _, err := range v.Errors() {
+			fmt.Println(err)
+		}
+		return
+	}
+
+	gen := generator.New()
+	output := gen.Generate(proto)
+
+	fmt.Println(output)
+}
