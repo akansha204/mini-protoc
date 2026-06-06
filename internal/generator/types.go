f8fcf520dc994a00fec46e63449786b05fc86ab1
Akansha Roy <akansharoy2008@gmail.com>
2026-06-06 16:17:31 +0530

    feat(generator) : type map acc to go

---
+++ internal/generator/types.go
@@ -0,0 +1,10 @@
+package generator
+
+var typeMap = map[string]string{
+	"string": "string",
+	"int32":  "int32",
+	"int64":  "int64",
+	"bool":   "bool",
+	"float":  "float32",
+	"double": "float64",
+}
