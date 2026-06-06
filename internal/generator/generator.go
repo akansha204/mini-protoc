d5a4ce5233c87a4e17dc493b15cd276f8a7e01fa
Akansha Roy <akansharoy2008@gmail.com>
2026-06-06 16:11:48 +0530

    feat(generator) : generating pkg naming

--- internal/generator/generator.go
+++ internal/generator/generator.go
@@ -1,1 +1,27 @@
 package generator
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/akansha204/mini-protoc/internal/ast"
+)
+
+type Generator struct {
+	builder strings.Builder
+}
+
+func New() *Generator {
+	return &Generator{}
+}
+
+func (g *Generator) Generate(proto *ast.ProtoFile) string {
+	g.generatePackage(proto.Package)
+
+	return g.builder.String()
+}
+
+func (g *Generator) generatePackage(pkg string) {
+	pkg = strings.ReplaceAll(pkg, ".", "_")
+	g.builder.WriteString(fmt.Sprintf("package %s\n\n", pkg))
+}
