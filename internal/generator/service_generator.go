9cd623d952e24cddf3082757a460e9541efaf952
Akansha Roy <akansharoy2008@gmail.com>
2026-06-11 23:20:50 +0530

    feat(generator) : registered method is now generated by compiler

--- internal/generator/service_generator.go
+++ internal/generator/service_generator.go
@@ -69,7 +69,46 @@ func (g *Generator) generateService(service *ast.Service) {
 		service.Name,
 	)
 
-	g.builder.WriteString("\tpanic(\"not implemented\")\n")
+	for _, rpc := range service.RPC {
+
+		fmt.Fprintf(
+			&g.builder,
+			"\tserver.Register(\n",
+		)
+
+		fmt.Fprintf(
+			&g.builder,
+			"\t\t%q,\n",
+			service.Name+"/"+rpc.Name,
+		)
+
+		g.builder.WriteString("\t\tfunc(payload []byte) ([]byte, error) {\n\n")
+
+		fmt.Fprintf(
+			&g.builder,
+			"\t\t\tvar req %s\n\n",
+			rpc.RequestType,
+		)
+
+		g.builder.WriteString("\t\t\tif err := server.Decode(payload, &req); err != nil {\n")
+		g.builder.WriteString("\t\t\t\treturn nil, err\n")
+		g.builder.WriteString("\t\t\t}\n\n")
+
+		fmt.Fprintf(
+			&g.builder,
+			"\t\t\tresp, err := service.%s(req)\n",
+			rpc.Name,
+		)
+
+		g.builder.WriteString("\t\t\tif err != nil {\n")
+		g.builder.WriteString("\t\t\t\treturn nil, err\n")
+		g.builder.WriteString("\t\t\t}\n\n")
+
+		g.builder.WriteString("\t\t\treturn server.Encode(resp)\n")
+
+		g.builder.WriteString("\t\t},\n")
+		g.builder.WriteString("\t)\n\n")
+	}
 
 	g.builder.WriteString("}\n\n")
 }
